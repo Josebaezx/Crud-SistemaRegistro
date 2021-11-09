64ec2687eb4f3bdce92f1bb8aa4a87bfc7bfeb85
Josebaezx <jose_baez84@hotmail.com>
2021-11-09 10:01:13 -0300

    Se mejora la busqueda con Query native y se valida buscar con js

--- src/main/java/py/com/jbx/solo/model/dao/IEmpleadoDao.java
+++ src/main/java/py/com/jbx/solo/model/dao/IEmpleadoDao.java
@@ -9,7 +9,8 @@ import py.com.jbx.solo.model.entity.Empleado;
 
 public interface IEmpleadoDao extends JpaRepository<Empleado, Long> {
 	
-	@Query("select p from Empleado p where p.nombre like %?1% ")
+	//@Query("select p from Empleado p where p.nombre like %?1% ")
+	@Query(value= "select * from empleados where nombre ilike %?1% ", nativeQuery = true)
 	public List<Empleado> findEmpleadoByNombre(String nombre);
 	
 }
